c42b9cd225ddf58bf864a96067aa67b57515c152
zhuzhengwei <zhuzhengwei921@jk.cn>
2016-05-03 10:03:03 +0800

    mysql pipeline

--- Sina_spider2/Sina_spider2/pipelines.py
+++ Sina_spider2/Sina_spider2/pipelines.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import pymongo
 from items import InformationItem, TweetsItem
-
+import json
+import MySQLdb
+import settings
+import sys
 
 class MongoDBPipleline(object):
     def __init__(self):
@@ -25,3 +28,71 @@ class MongoDBPipleline(object):
             except Exception:
                 pass
         return item
+
+class ScrapyWeiboPipeline(object):
+    def __init__(self):
+        try:
+            self.db = MySQLdb.connect(settings.MYSQL_HOST, settings.USER_NAME, settings.PASSWORD, settings.DATABASE,
+                                      charset="utf8")
+            self.cursor = self.db.cursor()
+        except MySQLdb.Error, e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print "File \"%s\", line %s. \n MySQL Error:%s" % (exc_traceback.tb_frame.f_code.co_filename,
+                                                               exc_traceback.tb_lineno, e)
+
+    def __del__(self):
+        try:
+            info = "close database."
+            self.db.close()
+        except (AttributeError, MySQLdb.Error), e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            info =  "Cound not destrut the ScrapWeiboPipeline.Object.\nFile \"%s\", line %s.\n MySql Error:%s" \
+                  % (exc_traceback.tb_frame.f_code.co_filename, exc_traceback.tb_lineno, e)
+
+
+    def process_item(self, item, spider):
+        if isinstance(item, InformationItem):
+            try:
+                self._insertUserItem(item)
+            except Exception:
+                pass
+        elif isinstance(item, TweetsItem):
+            try:
+                self._insertWeiboItem(item)
+            except Exception:
+                pass
+        return item
+
+    def _insertUserItem(self,item):
+        uname = item['name'].encode("utf-8")
+
+        sql = u"INSERT INTO sina.user(uid, uname, gender,province,city,signature,birthday, num_tweets,num_follows,num_fans,sex_orientation,marriage,url) " \
+              u"VALUES(%s, %s, %s, %s,%s,%s, %s, %s, %s,%s,%s, %s, %s)"
+        try:
+            self.cursor.execute(sql, (item['_id'], item['NickName'], item['Gender'],
+                                      item['Province'], item['City'], item['Signature'], item['Birthday'], item['Num_Tweets'],
+                                      item['Num_Follows'], item['Num_Fans'], item['Sex_Orientation'], item['Marriage'], item['URL']))
+            self.db.commit()
+        except (AttributeError, MySQLdb.Error), e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print "File \"%s\", line %s.\n MySql Error:%s" % (exc_traceback.tb_frame.f_code.co_filename,
+                                                              exc_traceback.tb_lineno, e)
+    def _insertWeiboItem(self, item):
+        """
+        Insert a WeiboItem into table in database
+        :param item: WeiboItem
+        :return: None
+        """
+
+        content = item['Content'].encode("utf-8")
+        sql = u"INSERT INTO sina.feed(uid_weibo_id, uid, content, pubtime, co_oridinates, tools, zan, comments, transfers) " \
+              u"VALUES(%s, %s, %s, %s,%s,%s, %s, %s, %s)"
+        try:
+            self.cursor.execute(sql, (item['_id'],item['ID'], content, item['PubTime'], item['Co_oridinates'],
+                                      item['Tools'],item['Like'],item['Comment'],item['Transfer']))
+            self.db.commit()
+        except (AttributeError, MySQLdb.Error), e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print "File \"%s\", line %s.\n MySql Error:%s" % (exc_traceback.tb_frame.f_code.co_filename,
+                                                              exc_traceback.tb_lineno, e)
+
